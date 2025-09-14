4d4cade7296076fcecf308281cd97516a5095168
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2025-09-14 00:05:14 -0400

    Update twoIntegerSumII.java

--- javaLeetcodeSolutions/src/javaLeetcodeSolutions/twoIntegerSumII.java
+++ javaLeetcodeSolutions/src/javaLeetcodeSolutions/twoIntegerSumII.java
@@ -1,5 +1,17 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        
+        int rightIndex = numbers.length - 1;
+        int leftIndex = 0;
+
+        while(numbers[leftIndex] + numbers[rightIndex] != target)
+        {
+            int currentSum = numbers[leftIndex] + numbers[rightIndex];
+            if(currentSum > target)
+                rightIndex--;
+            else
+                leftIndex++;
+        }
+
+        return new int[]{leftIndex + 1, rightIndex + 1};
     }
 }
